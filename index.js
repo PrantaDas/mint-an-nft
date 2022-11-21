5b95a047e17d72bcbc583f9c7a9fdb39e654e43d
Prantadas7 <pranta_das@coredevs.ltd>
2022-11-21 14:40:20 +0600

    removed unnecessary logs

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const Web3 = require('web3-eth');
-const web3 = new Web3('ws://localhost:7545')
+const web3 = new Web3('ws://localhost:7545');
 require('dotenv').config();
 const abi = require('./abi.json');
 const contractAddress = process.env.CONTRACT_ADDRESS;
 const myContract = new web3.Contract(abi, contractAddress);
 web3.defaultAccount = process.env.DEFAULT_WALLET_ADDRESS;
 (async () => {
-
     const wallet = '0xA17863802dDc5380f12073782bA109Fa8382aB43';
     let address;
     // get owner of the contract
