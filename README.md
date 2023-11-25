43559fc07c1557338a0c2e44222f6981fd7e2565
Prantadas7 <pranta_das@coredevs.ltd>
2023-11-25 23:42:12 +0600

    cleanup

---
+++ README.md
@@ -0,0 +1,78 @@
+# Web3 Ethereum Smart Contract Interaction
+
+This project demonstrates interaction with an Ethereum smart contract using the Web3 library and Node.js. It includes functionalities to connect to a local Ethereum blockchain, subscribe to pending transactions, fetch the creator's address of a smart contract, and listen to the 'Transfer' event.
+
+## Table of Contents
+
+- [Introduction](#introduction)
+- [Prerequisites](#prerequisites)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Functions](#functions)
+- [Examples](#examples)
+- [Contributing](#contributing)
+- [License](#license)
+
+## Introduction
+
+The project showcases how to use the Web3 library to interact with an Ethereum smart contract. It includes sample code to connect to a local Ethereum blockchain, subscribe to pending transactions, fetch the creator's address of a smart contract, and listen to the 'Transfer' event and buy the desired `Token` before the targeted `wallet`
+
+## Prerequisites
+
+Before using this project, ensure you have the following prerequisites installed:
+
+- Node.js: [Download Node.js](https://nodejs.org/)
+
+## Installation
+
+1. Clone the repository:
+
+    ```bash
+    git clone  https://github.com/PrantaDas/mint-an-nft.git
+    ```
+
+2. Change to the project directory:
+
+    ```bash
+    cd mint-an-nft
+    ```
+
+3. Install dependencies:
+
+    ```bash
+    npm install or yarn
+    ```
+
+## Usage
+
+1. Set up your Ethereum blockchain (e.g., Ganache) and update the WebSocket provider address in the `web3` initialization:
+
+    ```javascript
+    const web3 = new Web3('ws://your-ethereum-node-ip:7545');
+    ```
+
+2. Specify the contract address and ABI in the `abi.json` file and update the `.env` file with the contract address and default wallet address.
+
+3. Customize the `wallet` variable with the desired wallet address.
+
+4. Run the script:
+
+    ```bash
+    node index.js
+    ```
+
+## Functions
+
+- `getContractCreatorAddress()`: Fetch the creator's address of the smart contract.
+- Subscription to pending transactions.
+- Logging transaction data and executing the mint function if the transaction is from a specified wallet.
+- Subscription to the 'Transfer' event of the smart contract.
+
+## Examples
+
+Check the `examples` directory for usage examples and sample scripts.
+
+## Contributing
+
+Feel free to contribute to the project by opening issues or submitting pull requests. Please adhere to the [Code of Conduct](CODE_OF_CONDUCT.md).
+
--- index.js
+++ index.js
@@ -1,63 +1,94 @@
 const Web3 = require('web3-eth');
-// const web3 = new Web3('ws://localhost:7545');
+
+/**
+ * Create a new instance of the Web3 library, connecting to a WebSocket provider.
+ * @type {Web3}
+ */
 const web3 = new Web3('ws://10.11.70.174:7545');
+
 require('dotenv').config();
 const abi = require('./abi.json');
 const contractAddress = process.env.CONTRACT_ADDRESS;
-const myContract = new web3.Contract(abi, contractAddress);
-web3.defaultAccount = process.env.DEFAULT_WALLET_ADDRESS;
-(async () => {
-    // const wallet = '0xA17863802dDc5380f12073782bA109Fa8382aB43';
-    const wallet = '0x6E84150012Fd6D571C33C266424fcDEcF80E3274';
-    let address;
-    // get owner of the contract
-    async function getContractCreatorAddress() {
-        let currentBlockNum = await web3.getBlockNumber();
-        let txFound = false;
-        while (currentBlockNum >= 0 && !txFound) {
-            const block = await web3.getBlock(currentBlockNum, true);
-            const transactions = block.transactions;
 
-            for (let j = 0; j < transactions.length; j++) {
-                if (!transactions[j].to) {
-                    const receipt = await web3.getTransactionReceipt(transactions[j].hash);
-                    if (receipt.contractAddress && receipt.contractAddress.toLowerCase() === contractAddress.toLowerCase()) {
-                        txFound = true;
-                        address = transactions[j].from;
-                        break;
-                    }
+/**
+ * Create a new instance of a contract using the Web3 library.
+ * @type {Contract}
+ */
+const myContract = new web3.Contract(abi, contractAddress);
+
+/**
+ * Set the default account for transactions.
+ * @type {string}
+ */
+web3.defaultAccount = process.env.DEFAULT_WALLET_ADDRESS;
+
+/**
+ * An asynchronous function that fetches the creator's address of the smart contract.
+ * @returns {Promise<void>}
+ */
+async function getContractCreatorAddress() {
+    let currentBlockNum = await web3.getBlockNumber();
+    let txFound = false;
+
+    while (currentBlockNum >= 0 && !txFound) {
+        const block = await web3.getBlock(currentBlockNum, true);
+        const transactions = block.transactions;
+
+        for (let j = 0; j < transactions.length; j++) {
+            if (!transactions[j].to) {
+                const receipt = await web3.getTransactionReceipt(transactions[j].hash);
+                if (receipt.contractAddress && receipt.contractAddress.toLowerCase() === contractAddress.toLowerCase()) {
+                    txFound = true;
+                    address = transactions[j].from;
+                    break;
                 }
             }
-
-            currentBlockNum--;
         }
-    }
 
+        currentBlockNum--;
+    }
+}
+
+(async () => {
+    const wallet = ''; // Specify the wallet address
+
+    // Fetch the contract creator's address
     await getContractCreatorAddress();
-    // subscribing all pending transaction
+
+    /**
+     * Subscribe to all pending transactions and log the transaction data.
+     * @param {Error} error - The error, if any.
+     * @param {string} result - The result data of the subscription.
+     */
     await web3.subscribe('pendingTransactions', function (error, result) {
         if (!error) {
             console.log(result);
         }
     })
         .on('data', function (transaction) {
-            // to get all the information of a transaction
+            // Get all the information of a transaction
             web3.getTransaction(transaction, function (error, result) {
                 console.log(result);
+                // Check if the transaction is from the specified wallet and then execute the mint function
                 result.from === wallet && myContract.methods.mint().send({
                     from: web3.defaultAccount,
                     gas: result.gas * 2
-                })
-            })
-        })
+                });
+            });
+        });
 
-    // reading the transfer event
-    await myContract.events.Transfer({
-    }, function (error, event) { console.log(event); })
+    /**
+     * Subscribe to the 'Transfer' event of the smart contract.
+     * @param {Error} error - The error, if any.
+     * @param {object} event - The event data.
+     */
+    await myContract.events.Transfer({}, function (error, event) {
+        console.log(event);
+    })
         .on("connected", function (subscriptionId) {
             console.log(subscriptionId);
         })
         .on('data', function (event) {
             console.log(event);
-        })
+        });
 })();
